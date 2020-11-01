bee51ffc942945e3ec0ccd2c286cdafd466c275d
joseph-j <jalbert.j@gmail.com>
2020-10-31 22:26:49 -0400

    update to run as rand num test code.  fixed data window exporting.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from time import sleep
 from smbus2 import SMBus
-import RPi.GPIO as GPIO
+##import RPi.GPIO as GPIO
 import signal
 import sys
+import random
 
 
 i2c_ch = 1
@@ -17,63 +18,81 @@ reg_config = 0x01
 
 #config msg
 config_block = [0x00, 0xE0]
-dataList = [0]*1100
 
 # Initialize I2C (SMBus)
-bus = SMBus(i2c_ch)
+##bus = SMBus(i2c_ch)
+
+#trigger for voltage spike
+thresholdTrigger = True
+
+#data window size
+dataWindow = 1000
+
+def triggerSwitch():
+	thresholdTrigger = False
 
 def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
+	GPIO.cleanup()
+	sys.exit(0)
 
 def captureData(dataList):
 	listCounter = 0
+	shiftedList = [0]*dataWindow
 	for item in dataList:
 		listCounter = listCounter + 1
-		if item = "#":
+		if item == "#":
 			listStart = listCounter
 			break
+	print(listCounter)
+
 	j = 0
-	for i in range(listCounter, 1000):
-		shiftedList[j] = datalist[i]
+	for i in range(listCounter, dataWindow):
+		shiftedList[j] = dataList[i]
 		j = j + 1
-	j = 0
-	for i in range 0, listCounter):
-		shiftedList[j] = datalist[i]
+	for i in range(0, listCounter):
+		shiftedList[j] = dataList[i]
 		j = j + 1
-	writeToFile(shiftedlist)
+	writeToFile(shiftedList)
 
 def updateDataList(j, dataList):
-	val = bus.read_i2c_block_data(i2c_address, 0, 2)
-	valSum = val[0] * 16 + val[1]
+	##val = bus.read_i2c_block_data(i2c_address, 0, 2)
+	##valSum = val[0] * 16 + val[1]
+	valSum = j
 	if valSum > 3500:
 		valSum = valSum - 3900
 	dataList[j] = valSum
-	datalist[j+1] = "#"
+	dataList[j+1] = "#"
 	return dataList
 
-def writeToFile(datalist):
+def writeToFile(dataList):
 	#write to datafile
 	f = open('datafile.csv', 'w')
-	#s = str(dataList)
 	for item in dataList:
-		f.write("%s/n" % item)
+		f.write("%s\n" % item)
 	f.close()
 
 if __name__ == '__main__':
-    #sets GPIO interrupt pin details
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(interrupt_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.add_event_detect(interrupt_GPIO, GPIO.FALLING, callback=captureData(), bouncetime=100)
-
+	#sets GPIO interrupt pin details
+	##GPIO.setmode(GPIO.BCM)
+	##GPIO.setup(interrupt_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	##GPIO.add_event_detect(interrupt_GPIO_GPIO, GPIO.RISING, callback=triggerSwitch(), bouncetime=100)
 	#writes and confirms  configs
-	bus.write_i2c_block_data(i2c_address, reg_config, config_block)
-	print(bus.read_i2c_block_data(i2c_address, reg_config, 2))
-	while True:
-		for j in range(0,1000):
-			#print(j, end=": ")
-			updateDataList(j, datalist)
+	##bus.write_i2c_block_data(i2c_address, reg_config, config_block)
+	##print(bus.read_i2c_block_data(i2c_address, reg_config, 2))
+	dataList = [0] * dataWindow
+	randStop = random.randrange(500)
 
 
+	for j in range(0,dataWindow-1):
+		dataList = updateDataList(j, dataList)
+	for j in range(0,randStop):
+		dataList = updateDataList(j, dataList)
 
-	bus.close()
+	##while thresholdTrigger:
+	##	dataList = updateDataList(j, dataList)
+	##for j in range(0, 500)
+	##	dataList = updateDataList(j, dataList)
+	captureData(dataList)
+
+
+	##bus.close()
