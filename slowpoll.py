b4523915e978b55271bdfee9815ef9b61f5dddaa
joseph-j <jalbert.j@gmail.com>
2020-11-19 13:17:58 -0500

    fixed tab indent

--- slowpoll.py
+++ slowpoll.py
@@ -31,67 +31,67 @@ dataWindow = 1000
 
 
 def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
+	GPIO.cleanup()
+	sys.exit(0)
 
 
 def captureData(dataList):
-    fileCounter = dataList[0]
-    listCounter = 0
-    dataWindow = len(dataList)
-    shiftedList = [0] * dataWindow
-    listStart = 0
-    for item in dataList:
-        listCounter = listCounter + 1
-        if item == "#":
-            listStart = listCounter
-            break
-    j = 0
-    for i in range(listStart, dataWindow):
-        shiftedList[j] = dataList[i]
-        j = j + 1
-    for i in range(0, (listStart - 1)):
-        shiftedList[j] = dataList[i]
-        j = j + 1
-    writeToFile(shiftedList, fileCounter)
+	fileCounter = dataList[0]
+	listCounter = 0
+	dataWindow = len(dataList)
+	shiftedList = [0] * dataWindow
+	listStart = 0
+	for item in dataList:
+		listCounter = listCounter + 1
+		if item == "#":
+			listStart = listCounter
+			break
+	j = 0
+	for i in range(listStart, dataWindow):
+		shiftedList[j] = dataList[i]
+		j = j + 1
+	for i in range(0, (listStart - 1)):
+		shiftedList[j] = dataList[i]
+		j = j + 1
+	writeToFile(shiftedList, fileCounter)
 
 
 def updateDataList(j, dataList):
-    val = bus.read_i2c_block_data(i2c_address, 0, 2)
-    valSum = (val[0]) * 16 + val[1]
-    if valSum > 1000:
-        valSum = 4335 - valSum
-    dataList[j] = valSum
-    dataList[j + 1] = "#"
-    return dataList
+	val = bus.read_i2c_block_data(i2c_address, 0, 2)
+	valSum = (val[0]) * 16 + val[1]
+	if valSum > 1000:
+		valSum = 4335 - valSum
+	dataList[j] = valSum
+	dataList[j + 1] = "#"
+	return dataList
 
 
 def writeToFile(dataList, fileCounter):
-    # write to datafile
-    f = open('sp' + str(fileCounter) + '.csv', 'w')
-    print('wrote: sp' + str(fileCounter) + '.csv')
-    for item in dataList:
-        f.write("%s\n" % item)
-    f.close()
+	# write to datafile
+	f = open('sp' + str(fileCounter) + '.csv', 'w')
+	print('wrote: sp' + str(fileCounter) + '.csv')
+	for item in dataList:
+		f.write("%s\n" % item)
+	f.close()
 
 
 if __name__ == '__main__':
-    # sets GPIO interrupt pin details
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(interrupt_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    # GPIO.add_event_detect(interrupt_GPIO, GPIO.RISING, callback=triggerSwitch, bouncetime=100)
-    # writes and confirms  configs
-    bus.write_i2c_block_data(i2c_address, reg_config, config_block)
-    print(bus.read_i2c_block_data(i2c_address, reg_config, 16))
+	# sets GPIO interrupt pin details
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(interrupt_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	# GPIO.add_event_detect(interrupt_GPIO, GPIO.RISING, callback=triggerSwitch, bouncetime=100)
+	# writes and confirms  configs
+	bus.write_i2c_block_data(i2c_address, reg_config, config_block)
+	print(bus.read_i2c_block_data(i2c_address, reg_config, 16))
 
-    # writes and confirms threshold high values
-    bus.write_i2c_block_data(i2c_address, threshHi_config, threshHi_block)
-    print(bus.read_i2c_block_data(i2c_address, threshHi_config, 16))
-    dataSum = [0] * 1000
-    dataList = [0] * (dataWindow + 1)
-    threshHi_block = [62, 192]
-    bus.write_i2c_block_data(i2c_address, threshHi_config, threshHi_block)
-    bus.write_i2c_block_data(i2c_address, 0, [0, 0])
+	# writes and confirms threshold high values
+	bus.write_i2c_block_data(i2c_address, threshHi_config, threshHi_block)
+	print(bus.read_i2c_block_data(i2c_address, threshHi_config, 16))
+	dataSum = [0] * 1000
+	dataList = [0] * (dataWindow + 1)
+	threshHi_block = [62, 192]
+	bus.write_i2c_block_data(i2c_address, threshHi_config, threshHi_block)
+	bus.write_i2c_block_data(i2c_address, 0, [0, 0])
 	for n in range (0, 20):
 		for i in range(0, 500):
 			for j in range(1, dataWindow):
@@ -108,4 +108,4 @@ if __name__ == '__main__':
 			dataList = [0] * (dataWindow + 1)
 			#time.sleep(1)
 		writeToFile(dataSum, n)
-    bus.close()
+	bus.close()
