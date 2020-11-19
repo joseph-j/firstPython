6139aa1cf8af5fedc679ddeed150bb81856cf9e9
joseph-j <jalbert.j@gmail.com>
2020-11-19 03:42:45 +0000

    fixed logic issues.  working version

--- slowpoll.py
+++ slowpoll.py
@@ -57,27 +57,19 @@ def captureData(dataList):
 
 
 def updateDataList(j, dataList):
-    val2 = [0] * 2
     val = bus.read_i2c_block_data(i2c_address, 0, 2)
     valSum = (val[0]) * 16 + val[1]
     if valSum > 1000:
         valSum = 4335 - valSum
-    val2[0] = valSum
-    val = bus.read_i2c_block_data(i2c_address, 4, 2)
-    valSum = (val[0]) * 16 + val[1]
-    if valSum > 1000:
-        valSum = 4335 - valSum
-    val2[1] = valSum
-
-    dataList[j] = val2
+    dataList[j] = valSum
     dataList[j + 1] = "#"
     return dataList
 
 
 def writeToFile(dataList, fileCounter):
     # write to datafile
-    f = open('slowPoll' + str(fileCounter) + '.csv', 'w')
-    print('wrote: datafile' + str(fileCounter) + '.csv')
+    f = open('sp' + str(fileCounter) + '.csv', 'w')
+    print('wrote: sp' + str(fileCounter) + '.csv')
     for item in dataList:
         f.write("%s\n" % item)
     f.close()
@@ -87,7 +79,7 @@ if __name__ == '__main__':
     # sets GPIO interrupt pin details
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(interrupt_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(interrupt_GPIO, GPIO.RISING, callback=triggerSwitch, bouncetime=100)
+    # GPIO.add_event_detect(interrupt_GPIO, GPIO.RISING, callback=triggerSwitch, bouncetime=100)
     # writes and confirms  configs
     bus.write_i2c_block_data(i2c_address, reg_config, config_block)
     print(bus.read_i2c_block_data(i2c_address, reg_config, 16))
@@ -95,39 +87,25 @@ if __name__ == '__main__':
     # writes and confirms threshold high values
     bus.write_i2c_block_data(i2c_address, threshHi_config, threshHi_block)
     print(bus.read_i2c_block_data(i2c_address, threshHi_config, 16))
-    n = 0
-    dataSum = [0] * 10000
+    dataSum = [0] * 1000
     dataList = [0] * (dataWindow + 1)
-    dataList[0] = False
-    thresholdTrigger = dataList[0]
-    halfWindow = int(dataWindow / 2)
     threshHi_block = [62, 192]
     bus.write_i2c_block_data(i2c_address, threshHi_config, threshHi_block)
     bus.write_i2c_block_data(i2c_address, 0, [0, 0])
 
-    for i in range(0, 2400):
+    for i in range(0, 500):
         for j in range(1, dataWindow):
             dataList = updateDataList(j, dataList)
-            if dataList[0]:
-                for h in range(1, halfWindow):
-                    dataList = updateDataList(h, dataList)
-                j = dataWindow + 1
-                break
         dataListSum = 0
-        dataListSum2 = 0
-        for k in range(10, 400):
-            dataListNext = dataList[k][0]
+        for k in range(1, 500):
+            dataListNext = dataList[k]
             if isinstance(dataListNext, int):
                 dataListSum = dataListSum + dataListNext
-            dataListNext = dataList[k][1]
-            if isinstance(dataListNext, int):
-                dataListSum2 = dataListSum2 + dataListNext
-        print(dataListSum, ", ", dataListSum2)
-        dataSum[n] = dataListSum
-        n = n + 1
-        dataList[0] = i
-        captureData(dataList)
+        print(dataListSum)
+        dataSum[i] = dataListSum
+        # dataList[0] = i
+        # captureData(dataList)
         dataList = [0] * (dataWindow + 1)
-        time.sleep(1)
-    writeToFile(dataSum, 1)
+        #time.sleep(1)
+    writeToFile(dataSum, 2)
     bus.close()
